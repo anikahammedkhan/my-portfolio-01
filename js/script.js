838a77c76820c7d48089d4f2fa4bac8d684ba88c
anikahammedkhan <anikkhan1105@gmail.com>
2022-07-02 23:20:15 +0600

    my-first-portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==================================== typing animation =============================== */
+var typed =new Typed(".typing", {
+    strings:["Web Designer","Web Developer","MERN Stack Developer"],
+    typeSpeed: 150,
+    BackSpeed: 60,
+    loop:true
+})
